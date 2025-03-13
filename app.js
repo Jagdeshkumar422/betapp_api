97d0117ab7fb1f936e79ee1f5d2655395845e0a4
Jagdeshkumar422 <jk9876211@gmail.com>
2025-03-13 18:24:26 +0500

    asdfasdas

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require("cors");
+require('dotenv').config();
+const bodyParser = require("body-parser");
+const app = express();
+
+// Middleware for parsing JSON
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(cors({
+    origin: "*", // Replace with your frontend URL
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  }));
+// Register the routes
+
+app.get("/api", (req, res) => {
+  res.json({ message: "API running successfully" });
+});
+
+
+// Connect to MongoDB (replace with your own URI)
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+   
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
+
+const PORT = process.env.PORT || 5002;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
