ba2ebd1d80c2a6817ffa999d48ae16a67ac890ba
Jagdeshkumar422 <jk9876211@gmail.com>
2025-03-15 17:11:33 +0500

    adsdasd

--- models/verifycode.js
+++ models/verifycode.js
@@ -4,4 +4,4 @@ const verifySchema = new mongoose.Schema({
     betId: { type: String, required: true }, 
     verifyCode: { type: String, unique: true }  // Ensure verifyCode is unique
 });
-const VerifyModel = mongoose.model("VerifyCode", verifySchema);
+module.exports = mongoose.model("VerifyCode", verifySchema);
--- routes/verifyCodeRoute.js
+++ routes/verifyCodeRoute.js
@@ -42,4 +42,4 @@ router.put("/verify-code/:betId", async (req, res) => {
     }
 });
 
-module.exports =router
+module.exports = router
