34104e8a9dfa5b89ff5dcb05cacaa02a72271d9d
Jagdeshkumar422 <jk9876211@gmail.com>
2025-03-15 21:02:08 +0500

    adsdasd

--- models/oddModel.js
+++ models/oddModel.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const oddSchema = new mongoose.Schema({
     betId: { type: String, required: true }, 
-    odd: { type: String, unique: true }  // Ensure verifyCode is unique
+    odd: Number  // Ensure verifyCode is unique
 });
 
 module.exports = mongoose.model("odd", oddSchema)
