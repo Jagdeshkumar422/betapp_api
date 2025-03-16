f01ef728be98d0e069003c4da6836f1190854aec
Jagdeshkumar422 <jk9876211@gmail.com>
2025-03-16 08:22:10 +0500

    adsdasd

--- routes/betRoute.js
+++ routes/betRoute.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Bet = require("../models/bet");
+const Deposit =  require("../models/deposite")
 
 // Fetch Bets for Logged-in User
 router.get("/bets/:userId", async (req, res) => {
@@ -22,15 +23,24 @@ router.post("/bets", async (req, res) => {
       if (!userId || !date || !betCode || !stake) {
         return res.status(400).json({ error: "All fields are required" });
       }
+
   
       // Validate data types
       if (isNaN(stake) || stake <= 0) {
         return res.status(400).json({ error: "Invalid stake value" });
       }
-  
+
+      const deposit = await Deposit.findOne({ userId });
+
+    if (!deposit || deposit.amount < stake) {
+      return res.status(400).json({ message: "Insufficient balance" });
+    }
+    deposit.amount -= stake;
+    await deposit.save();
+    
       const newBet = new Bet({ userId, date, betCode, stake });
       const savedBet = await newBet.save();
-  
+    
       res.status(201).json(savedBet);
     } catch (error) {
       console.error("Error adding bet:", error.message);
